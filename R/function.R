5ac7be4868f2a223e3c35b41c05977e993c0321b
Maud QUEROUE <maud.queroue@gmail.com>
2020-12-16 17:03:54 +0100

    Mise à jour CLC_STOC

--- R/function.R
+++ R/function.R
@@ -72,14 +72,15 @@ give_point_by_site <- function(dsf_STOC, dsf_EPS, dist_km) {
 #' @return 
 #' @export
 #'
-plot_carte <- function(nb_STOC, dsf_STOC, dsf_EPS, CLC_EPS, dist_km, shp) {
-  
+plot_carte <- function(nb_STOC, dsf_STOC, CLC_EPS, dist_km, shp) {
   
   coord_buffer_STOC <- sf::st_buffer(dsf_STOC[nb_STOC,],dist=units::set_units(dist_km, km))
   
   # Coord Point STOC
   x <- as.vector(as(dsf_STOC[nb_STOC,], "Spatial")@coords)[1]
   y <- as.vector(as(dsf_STOC[nb_STOC,], "Spatial")@coords)[2]
+  
+  dsf_EPS <-  Mesanges::give_point(CLC_EPS)
   
   points <- Mesanges::give_point_by_site(dsf_STOC[nb_STOC,], dsf_EPS, dist_km)
   
@@ -103,6 +104,42 @@ plot_carte <- function(nb_STOC, dsf_STOC, dsf_EPS, CLC_EPS, dist_km, shp) {
           ggplot2::coord_sf(
             xlim = c((x-25100),(x+25100)),
             ylim = c((y-25100),(y+25100)),
+            datum = sf::st_crs(2154),
+            expand=FALSE)
+  )
+}
+
+#' Plot carte
+#'
+#' @param  nb_STOC, dsf_STOC, shp  
+#'
+#' @return 
+#' @export
+#'
+plot_carte_STOC <- function(nb_STOC, CLC_STOC, shp) {
+  
+  dsf_STOC <-  Mesanges::give_point(CLC_STOC)
+  
+  coord_buffer_STOC <- sf::st_buffer(dsf_STOC[nb_STOC,],dist=units::set_units(200, m))
+  
+  # Coord Point STOC
+  x <- as.vector(as(dsf_STOC[nb_STOC,], "Spatial")@coords)[1]
+  y <- as.vector(as(dsf_STOC[nb_STOC,], "Spatial")@coords)[2]
+  
+  #Polygones interessants
+  int <- sf::st_intersects(coord_buffer_STOC$geometry, shp_CLC)
+  
+  polygone <- shp_CLC %>%
+    dplyr::filter(shp_CLC$ID %in% as.character(shp_CLC$ID[int[[1]]]))
+  rm(int)
+  
+  print(ggplot2::ggplot() +
+          ggplot2::geom_sf(data = polygone, ggplot2::aes(fill=CODE_12))+ 
+          ggplot2::geom_sf(data = coord_buffer_STOC, ggplot2::aes(fill=NA),colour="white",alpha=0.1) +
+          ggplot2::geom_sf(data = dsf_STOC[nb_STOC,],colour="white",size=5) +
+          ggplot2::coord_sf(
+            xlim = c((x-300),(x+300)),
+            ylim = c((y-300),(y+300)),
             datum = sf::st_crs(2154),
             expand=FALSE)
   )
