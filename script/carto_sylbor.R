c6ce5666317cb7c5f6b733ceda94913beabb6419
Maud QUEROUE <maud.queroue@gmail.com>
2021-06-21 15:08:06 +0200

    test

--- script/carto_sylbor.R
+++ script/carto_sylbor.R
@@ -18,28 +18,94 @@ Fr <- sf::st_read(dsn = here::here('data','France'),
   dplyr::group_by(PAYS)%>% 
   dplyr::summarize()
 
+# DATA MNHN
 
-# Data STOC
-#------------------
+data_brut <- read.csv2(here::here('Data',"Data_MNHN.csv")) %>%
+  dplyr::select('ACTION','BAGUE','DATE','HEURE','ESPECE','SEXE','AGE',"THEME.SESSION","ID_PROG","LAT","LON","DEPT","LOCALITE")
 
-#data
-load(here::here("output","data_STOC.RData"))
+data <- data_brut 
 
-#ajout colonne annee
-data_STOC$year <- substr(data_STOC$DATE,7,10)
+rm(data_brut)
+#-- 534407 
 
-# On garde que sylbor
-sylbor_STOC <- subset(data_STOC,data_STOC$ESPECE=="SYLBOR") 
+####### ATTENTION MODIF DATA #####
+# -> station 43 et 623 ? lier
+# -> station 42 et 285 ? lier 
+# -> station 200 et 615 ? lier
+data$ID_PROG[which(data$ID_PROG==43)]<-623
+data$ID_PROG[which(data$ID_PROG==42)]<-285
+data$ID_PROG[which(data$ID_PROG==200)]<-615
 
-# enlever les lignes sans nom de site ou annee
-sylbor_STOC <- sylbor_STOC %>% 
-  dplyr::filter(!is.na(ID_PROG)) %>%
-  dplyr::filter(!is.na(year))
+# on supprime data sans date, sans ID_PROG,sas espece
+data <- data %>% 
+  dplyr::filter(!is.na(ID_PROG))%>% 
+  dplyr::filter(!is.na(DATE)) %>% 
+  dplyr::filter(!is.na(ESPECE))
 
-# Cb de sylbor par an par annee
+
+#1 ON NE GARDE QUE LES DONNES STOC et STOC ROZO
+# -----------------------------------------------------------------------
+
+unique(data$THEME.SESSION)
+
+data <- data %>%
+  dplyr::filter(data$THEME.SESSION == "STOC" | data$THEME.SESSION == "STOC ROZO")
+
+unique(data$THEME.SESSION)
+# -- 503904
+
+# 2 QUELLES ANNEES GARDE T ON ?
+#------------------------------------------------------------------------
+
+# On ne garde que les annees apres 2001 pour etre coherent avec les donnees EPS
+unique(substr(data$DATE,7,10))
+
+data <- data %>%
+  dplyr::filter(substr(data$DATE,7,10) > 2000) 
+
+unique(substr(data$DATE,7,10))
+#-- 422610
+
+data$year <- substr(data$DATE,7,10)
+
+# 2 QUELS MOIS GARDE T ON ?
+#------------------------------------------------------------------------
+unique(substr(data$DATE,4,5))
+
+data <- data %>%
+  dplyr::filter(substr(data$DATE,4,5) %in% c("04", "05", "06", "07")) 
+
+unique(substr(data$DATE,4,5))
+
+# On reformate les données
+#------------------------------------------------------------------------
+data_STOC <- data %>% 
+  dplyr::group_by(ID_PROG,DATE,ESPECE,year) %>%
+  dplyr::summarise(n=dplyr::n()) %>%
+  tidyr::pivot_wider(names_from = ESPECE, values_from = n) 
+
+# on range dans l'ordre croissant des dates
+data_STOC$DATE <- lubridate::dmy(data_STOC$DATE)
+data_STOC  <- data_STOC %>%
+  dplyr::arrange(DATE)
+
+data_STOC[is.na(data_STOC)] <- 0
+
+# On garde que les sylbor
+#------------------------------------------------------------------------
+sylbor_STOC <- data_STOC %>%
+  dplyr::select('ID_PROG','DATE','year','SYLBOR')
+
+
+# Combien de sylbor par site par an ? 
+#------------------------------------------------------------------------
 sylbor_STOC <- sylbor_STOC %>%
   dplyr::group_by(ID_PROG,year) %>%
-  dplyr::summarise(n=dplyr::n())
+  dplyr::summarise(n=sum(SYLBOR))
+
+sylbor_STOC <- sylbor_STOC %>%
+  dplyr::group_by(ID_PROG) %>%
+  dplyr::summarise(n=mean(n))
 
 # Les differents points avec leur longitude et latitude et type CLC
 CLC_STOC <- read.table(here::here("output","CLC_STOC.csv"),head=T,sep=";") %>%
@@ -55,10 +121,12 @@ dsf_STOC <-  CLC_STOC %>%
   sf::st_as_sf(coords = c("long","lat"), crs = 4326) %>%
   sf::st_transform(crs = 4326)
 
+dsf_STOC_P <- dsf_STOC[which(dsf_STOC$n>0),]
+
 # Plot carte
 ggplot2::ggplot(Fr) +
   ggplot2::geom_sf() +
-  ggplot2::geom_sf(data = dsf_STOC, ggplot2::aes(color=n)) +
+  ggplot2::geom_sf(data = dsf_STOC_P, ggplot2::aes(color=n)) +
   ggplot2::coord_sf(crs = 4326, datum = sf::st_crs(4326)) +
   ggplot2::guides(fill = FALSE) +
   ggplot2::ggtitle("France") +
@@ -73,16 +141,25 @@ load(here::here("output","data_EPS.RData"))
 
 data_EPS <- data_EPS[,c('annee','carre','point','SYLBOR')]
 
-# on garde les point avec de la sybor
-sylbor_EPS <- subset(data_EPS,data_EPS$SYLBOR>0)
+# on supprime data sans date, sans point
+data_EPS <- data_EPS %>% 
+  dplyr::filter(!is.na(point))%>% 
+  dplyr::filter(!is.na(annee))
 
-lien_carre_point <- sylbor_EPS %>%
+data_EPS[is.na(data_EPS)] <- 0
+
+lien_carre_point <- data_EPS %>%
   dplyr::distinct(carre,.keep_all = TRUE) 
+
 lien_carre_point <- lien_carre_point[,c('carre','point')]
 
-sylbor_EPS <- sylbor_EPS %>%
+sylbor_EPS <- data_EPS %>%
   dplyr::group_by(annee,carre)  %>%
-  dplyr::summarise(n=dplyr::n())
+  dplyr::summarise(n=sum(SYLBOR))
+
+sylbor_EPS <- sylbor_EPS %>%
+  dplyr::group_by(carre) %>%
+  dplyr::summarise(n=mean(n))
 
 # Les differents points avec leur longitude et latitude et type CLC
 load(here::here("output","CLC_EPS.RData"))
@@ -105,10 +182,12 @@ dsf_EPS <-  CLC_EPS %>%
   sf::st_as_sf(coords = c("long","lat"), crs = 4326) %>%
   sf::st_transform(crs = 4326)
 
+dsf_EPS_P <- dsf_EPS[which(dsf_EPS$n>0),]
+
 # Plot EPS
 ggplot2::ggplot(Fr) +
   ggplot2::geom_sf() +
-  ggplot2::geom_sf(data = dsf_EPS, ggplot2::aes(color=n)) +
+  ggplot2::geom_sf(data = dsf_EPS_P, ggplot2::aes(color=n)) +
   ggplot2::coord_sf(crs = 4326, datum = sf::st_crs(4326)) +
   ggplot2::guides(fill = FALSE) +
   ggplot2::ggtitle("France") +
@@ -119,6 +198,6 @@ ggplot2::ggplot(Fr) +
 #----------------------------------------------
 library(gamm4)
 
-model <- gamm4(n~s(lat),family=poisson,data=CLC_EPS)
+model <- gamm4(round(n)~s(lat),family=poisson,data=CLC_EPS)
 
 plot.gam(model$gam)
--- script/gam_pheno.R
+++ script/gam_pheno.R
@@ -1,13 +1,3 @@
-######                                       ######
-###### SCRIPT PREMIER TRI DONNEES PASSEREAUX ######
-######                                       ######
-
-# Jeu de donnees entrant : donnees brut MNHN
-
-# Jeu de donnees sortant : histoire de vie des sites
-#                          Premier tri donnees MNHN         
-
-
 rm(list=ls())
 
 # Packages necessaires
@@ -34,10 +24,11 @@ data$ID_PROG[which(data$ID_PROG==43)]<-623
 data$ID_PROG[which(data$ID_PROG==42)]<-285
 data$ID_PROG[which(data$ID_PROG==200)]<-615
 
-
-# enlever les lignes sans nom de site
+# on supprime data sans date, sans ID_PROG,sas espece
 data <- data %>% 
-  dplyr::filter(!is.na(ID_PROG)) 
+  dplyr::filter(!is.na(ID_PROG))%>% 
+  dplyr::filter(!is.na(DATE)) %>% 
+  dplyr::filter(!is.na(ESPECE))
 
 
 #1 ON NE GARDE QUE LES DONNES STOC et STOC ROZO
@@ -51,19 +42,6 @@ data <- data %>%
 unique(data$THEME.SESSION)
 # -- 503904
 
-
-#1 ON NE GARDE QUE LES FAUVETTES SYLBOR et SYLATR
-# -----------------------------------------------------------------------
-
-unique(data$THEME.SESSION)
-
-data <- data %>%
-  dplyr::filter(data$ESPECE == "SYLATR" | data$ESPECE == "SYLBOR")
-
-unique(data$ESPECE)
-# -- 503904
-
-
 # 2 QUELLES ANNEES GARDE T ON ?
 #------------------------------------------------------------------------
 
@@ -76,23 +54,16 @@ data <- data %>%
 unique(substr(data$DATE,7,10))
 #-- 422610
 
-
-#3. Gestion date 
-#------------------------------------------------------
 data$annee <- substr(data$DATE,7,10)
 
+# 2 QUELS MOIS GARDE T ON ?
+#------------------------------------------------------------------------
 unique(substr(data$DATE,4,5))
 
 data <- data %>%
   dplyr::filter(substr(data$DATE,4,5) %in% c("04", "05", "06", "07")) 
 
 unique(substr(data$DATE,4,5))
-
-data$DATE <- lubridate::dmy(data$DATE)
-
-# on range dans l'ordre croissant des dates
-data <- data%>%
-  dplyr::arrange(DATE)
 
 #4. Gestion age 
 #------------------------------------------------------
@@ -103,6 +74,48 @@ data <- data[-which(data$new_AGE=="C"),]
 data <- data[-which(is.na(data$new_AGE)==TRUE),]
 
 unique(data$new_AGE)
+
+# On reformate les données
+#------------------------------------------------------------------------
+data_STOC <- data %>% 
+  dplyr::group_by(ID_PROG,DATE,ESPECE,annee,new_AGE) %>%
+  dplyr::summarise(n=dplyr::n()) %>%
+  tidyr::pivot_wider(names_from = ESPECE, values_from = n) 
+
+# on range dans l'ordre croissant des dates
+data_STOC$DATE <- lubridate::dmy(data_STOC$DATE)
+data_STOC  <- data_STOC %>%
+  dplyr::arrange(DATE)
+
+data_STOC[is.na(data_STOC)] <- 0
+
+data_STOC <- data_STOC %>%
+  dplyr::select('ID_PROG','DATE','annee','new_AGE','SYLATR','SYLBOR')
+
+######## tricherie date pour plot
+data_STOC$fDATE <- data_STOC$DATE
+
+for(i in 1:nrow(data_STOC)){
+  data_STOC$fDATE[i] <- update(data_STOC$fDATE[i],year = 2001)
+}
+
+### Date en chiffre a parti de mars
+num_Date <- seq(as.Date("2001-03-01"), as.Date("2001-07-31"), by="days")
+
+data_STOC$nDATE <- NA
+for (i in 1:nrow(data_STOC)){
+  data_STOC$nDATE[i] <- which(num_Date==data_STOC$fDATE[i])
+}
+
+data_STOC <- data_STOC %>%
+  dplyr::select(!'fDATE')
+
+data <- data_STOC %>% 
+  tidyr::pivot_longer(cols = c('SYLATR','SYLBOR'), names_to = c('ESPECE')) 
+
+data <- data %>% 
+  tidyr::pivot_wider(names_from = new_AGE, values_from = c('value')) 
+data[is.na(data)] <- 0
 
 
 #5. QUELLES STATIONS GARDER EN LATITUDE
@@ -118,71 +131,15 @@ ID_PROG <- CLC_STOC %>%
 
 ID_to_keep <- unique(ID_PROG$ID_PROG)
 
-data <- data %>%
-  dplyr::filter(data$ID_PROG %in% ID_to_keep)
+data <- data[which((data$ID_PROG %in% ID_to_keep)==TRUE),]
 
-rm(ID_to_keep,CLC_STOC,ID_PROG)
-
-#5. garde que indiv dans jeu de données CMR
-#----------------------------------------------
-
-#data <- subset(data, data$ESPECE=="SYLBOR" | data$ESPECE=="SYLATR" )
-
-
-load(here::here('output','hvie_sylatr_tot_tt.RData'))
-ID_sylatr <- unique(hvie_sylatr$ID)
-
-data_sylatr <- data %>%
-  dplyr::filter(data$BAGUE %in% ID_sylatr)
-unique(data_sylatr$ESPECE)
-
-
-load(here::here('output','hvie_sylbor_tot_tt.RData'))
-ID_sylbor <- unique(hvie_sylbor$ID)
-
-data_sylbor <- data %>%
-  dplyr::filter(data$BAGUE %in% ID_sylbor)
-unique(data_sylbor$ESPECE)
-
-# on crée un tableau commun
-data <- rbind(data_sylatr,data_sylbor)
-
-rm(ID_sylatr,ID_sylbor,hvie_sylatr,hvie_sylbor, data_sylatr,data_sylbor)
-
-#7. On garde premiere capture par an  
-#------------------------------------------------------
-
-#data_ind <- data %>% dplyr::distinct(annee,BAGUE,ESPECE,.keep_all = TRUE)
-data_ind <- data
-
-######## tricherie date pour plot
-data_ind$fDATE <- data_ind$DATE
-
-for(i in 1:nrow(data_ind)){
-  data_ind$fDATE[i] <- update(data_ind$fDATE[i],year = 2001)
-}
-
-### Date en chiffre a parti de mars
-num_Date <- seq(as.Date("2001-03-01"), as.Date("2001-07-31"), by="days")
-
-data_ind$nDATE <- NA
-for (i in 1:nrow(data_ind)){
-  data_ind$nDATE[i] <- which(num_Date==data_ind$fDATE[i])
-}
-
-
-data <- data_ind %>% 
-  #dplyr::group_by(annee,nDATE,ESPECE, new_AGE) %>%
-  dplyr::group_by(annee,nDATE,ESPECE, ID_PROG, new_AGE) %>%
-  dplyr::summarise(n=dplyr::n()) %>%
-  tidyr::pivot_wider(names_from = new_AGE, values_from = n) 
-
-data[is.na(data)==TRUE] <- 0
-
-rm(num_Date,i, data_ind)
+rm(ID_to_keep,CLC_STOC,ID_PROG,data_STOC,i,num_Date)
 
 ##################################################################
 
+test <- data   %>%
+  dplyr::group_by(DATE,ESPECE,annee,nDATE) %>%
+  dplyr::summarise(n=sum(c(A,P)))
 
 library(gamm4)
 
@@ -197,18 +154,17 @@ out3 <- data.frame('annee'=rep(0,Nombre_annees),"Decalage_pheno"=rep(0,Nombre_an
 a <- rep(0,Nombre_annees)
 a2 <- rep(0,Nombre_annees)
 
-
 # Boucle générale sur les années
 for (i in 1:Nombre_annees) {
   data_annee <- subset(data, annee==annees[i])
   out1 <- data.frame("annee"=rep(0,l),"DT"=rep(0,l),"AIC"=rep(0,l),"AIC2"=rep(0,l))
- 
+  
   # Boucle sur les décalages phénologiques testés
   for (h in 1:l){
     Decalage_test <- IntervalleTemps[h]
     data_annee$date <- rep(NA, nrow(data_annee))
     
-### Boucle permettant de créer les nouvelles dates décalées de SYLBOR en rajoutant le décalage 
+    ### Boucle permettant de créer les nouvelles dates décalées de SYLBOR en rajoutant le décalage 
     # phénologique testé sans toucher à SYLATR qui sert de référence. 
     # Ainsi, une valeur de décalage phénologique négative qu'on avance les données de SYLBOR 
     for (j in 1:nrow(data_annee)){
@@ -220,7 +176,7 @@ for (i in 1:Nombre_annees) {
       }
     }
     
-### Ajustement du modèle gamm à changer en fonction de vos données 
+    ### Ajustement du modèle gamm à changer en fonction de vos données 
     # (ici, le modèle ne contient pas d'effet site). 
     # Attention, si le modèle change, il est probable que les indices des valeurs à récupérer 
     # dans la matrice de résultats du modèle changent aussi. 
@@ -235,20 +191,20 @@ for (i in 1:Nombre_annees) {
     # plus utile
     rm(model,Decalage_test,j)
   }
-
-### Partie du programme qui sélectionne les 10 valeurs d'AIC autour du minimum 
+  
+  ### Partie du programme qui sélectionne les 10 valeurs d'AIC autour du minimum 
   # pour ensuite opérer une régression quadratique et estimer le décalage phénologique
   out2 <- data.frame('annee' =rep(0,11),'AIC'=rep(0,11),'DT'=rep(0,11))
-
+  
   # Recherche du minimum d'AIC et du décalage correspondant
   a[i] <- which(out1$AIC==min(out1$AIC))
-  a2[i] <- which(out1$AIC==min(out1$AIC))
+  a2[i] <- which(out1$AIC2==min(out1$AIC2))
   # Sélection des 10 valeurs d'AIC et des décalages correspondant en fonction de la position du minimum dans la série de décalages testés. 
   out2$AIC   <- signif(out1$AIC[(a[i]-5):(a[i]+5)],digit=6)
   out2$DT    <- out1$DT[(a[i]-5):(a[i]+5)]
   out2$annee <- annees[i]
   
-### Partie du programme qui opère une régression quadratique pour estimer le décalage phénologique entre les données de sylbor et sylatr
+  ### Partie du programme qui opère une régression quadratique pour estimer le décalage phénologique entre les données de sylbor et sylatr
   DT_carre <- out2$DT * out2$DT
   Regression_quadratique <- summary(glm(out2$AIC ~ DT_carre + out2$DT)) 
   
@@ -278,4 +234,8 @@ axis(1, at=seq(1.1,19.1,1), labels=seq(2001,2019,1))
 axis(2, las=2)
 ?legend 
 ?axis
-plot.gam(model$gam)
+
+DP <- out3$Decalage_pheno
+save(DP,file = here::here('output',"DP_es_ad.RData"))
+load(here::here('output','DP.RData'))
+load(here::here('output','DP_es.RData'))
